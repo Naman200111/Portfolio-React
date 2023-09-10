d76de390c2370e737ee1d70778f57d632564333a
Naman Agrawal <namanagrawal1895@gmail.com>
2023-09-10 19:52:56 +0530

    blank commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import Header from "./components/header/header"
 import Sections from "./components/sections/sections"
 import Footer from "./components/footer/footer"
-
 export default function App() {
   return (
     <div className="app-body">
